55006f1d4f18d5b9b74c7abd198e99178440b361
akshayojha <akshayojhacse@gmail.com>
2017-05-10 07:07:23 -0400

    Add Intial tester logic

--- src/tester/tester.go
+++ src/tester/tester.go
@@ -6,12 +6,19 @@ import (
 	"os"
 )
 
+// EmptyStr - string to denote empty string
+var EmptyStr = ""
+
 func main() {
 	// Get required information in command line from user
 
 	// Scheduler information
 	testerIPPtr := flag.String("tip", "localhost", "IP address of the tester")
-	testerPortPtr := flag.String("tport", "8080", "Port of the tester")
+	testerPortPtr := flag.String("tport", "0", "Port of the tester")
+
+	// Scheduler information required to setup the server
+	serverIPPtr := flag.String("sip", "localhost", "IP address of the scheduler server")
+	serverPortPtr := flag.String("sport", "8080", "Port of the scheduler server")
 
 	// Local repository path to observe
 	repoPathPtr := flag.String("rpath", EmptyStr, "Path to the repository folder to observe")
